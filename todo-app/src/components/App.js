d5236ac5631775be87591b41e5109af9ae802e9d
SrestMilan <shrestham780@gmail.com>
2023-10-10 21:02:17 +0545

    new component added ListItem

--- todo-app/src/components/App.js
+++ todo-app/src/components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Header";
 import Form from "./Form";
+import ListItem from "./ListItem";
 export default function App() {
   return (
     <>
       <Header />
       <Form />
+      <ListItem />
     </>
   );
 }
