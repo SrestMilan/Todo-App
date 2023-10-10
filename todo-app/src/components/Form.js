7027bb07083b40149bf0296cd14653177c255aef
SrestMilan <shrestham780@gmail.com>
2023-10-10 19:25:16 +0545

    working with state

--- todo-app/src/components/Form.js
+++ todo-app/src/components/Form.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Form() {
+  const [task, setTask] = useState("");
+  function formHandle(e) {
+    e.preventDefault();
+
+    setTask("");
+  }
   return (
-    <div>
-      <input type="text" placeholder="add task" />
-    </div>
+    <form className="form" onSubmit={formHandle}>
+      <div>
+        <input
+          type="text"
+          placeholder="add task"
+          value={task}
+          onChange={(texts) => setTask(texts.target.value)}
+        />
+        <button>ADD</button>
+      </div>
+    </form>
   );
 }
 
