0fa214ac2bbe8e41bf3740df69e5ef30153dfe6c
SrestMilan <shrestham780@gmail.com>
2023-10-12 16:06:06 +0545

    completed task funcionality

--- todo-app/src/components/Item.js
+++ todo-app/src/components/Item.js
@@ -1,25 +1,28 @@
 function Item({ listItem, onDeleteTask, onHandleCompletedTask }) {
   return (
-    <div className="taskList">
-      <li>
-        <div className="items">
-          <input
-            type="checkbox"
-            value={listItem.completed}
-            onChange={() => onHandleCompletedTask(listItem.id)}
-          />
+    <>
+      <div className="taskItem">
+        <input
+          type="checkbox"
+          value={listItem.completed}
+          onChange={() => onHandleCompletedTask(listItem.id)}
+        />
+
+        <div className="child child-2">
           <span
             style={listItem.completed ? { textDecoration: "line-through" } : {}}
           >
             {listItem.task}
           </span>
+        </div>
 
+        <div className="child child-3">
           <button className="delete" onClick={() => onDeleteTask(listItem.id)}>
             ❌
           </button>
         </div>
-      </li>
-    </div>
+      </div>
+    </>
   );
 }
 
